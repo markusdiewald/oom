07c1354063eff7b3a968f414ca5c470dd42d0ecc
markusdiewald <markus.diewald@hotmail.com>
2016-02-29 23:18:21 +0100

    Signed-off-by: markusdiewald <markus.diewald@hotmail.com>

---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stats = new[]
+            {
+                new Stats("Mistelbach Mustangs", "St. Pölten Dragons", 98, 76),
+                new Stats("Mistelbach Mustangs", "DC Timberwolves Vienna", 102, 98),
+            };
+
+            foreach (var x in stats)
+            {
+                Console.WriteLine("{0}:{1} {2}:{3}\n", x.Hometeam, x.Awayteam, x.Score_Hometeam, x.Score_Awayteam);
+            }
+
+
+        }
+    }
+}
---
+++ tasks/Task2/Task2/Stats.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Stats
+    {
+        public Stats(string hometeam, string awayteam, int score_hometeam, int score_awayteam)
+        {
+            if (string.IsNullOrWhiteSpace(hometeam)) throw new ArgumentException("Hometeam must not be empty", nameof(hometeam));
+            if (string.IsNullOrWhiteSpace(awayteam)) throw new ArgumentException("Awayteam must not be empty", nameof(awayteam));
+
+            Hometeam = hometeam;
+            Awayteam = awayteam;
+            Score_Hometeam = score_hometeam;
+            Score_Awayteam = score_awayteam;
+        }
+
+        public string Hometeam { get; }
+
+        public string Awayteam { get; }
+
+        public int Score_Hometeam { get; }
+
+        public int Score_Awayteam { get; }
+    }
+}
+
