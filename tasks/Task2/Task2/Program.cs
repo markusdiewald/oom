982be397b00dd43ed6d747958c185ecd2acbecfc
Markus Diewald <markuks.diewald@hotmail.com>
2017-03-19 21:47:54 +0100

    removed unnecessary comment block

--- tasks/Task2/Task2/Program.cs
+++ tasks/Task2/Task2/Program.cs
@@ -11,19 +11,6 @@ using Newtonsoft.Json;
 namespace Task2
 {
 
-    /*public class RootObject
-    {
-        public int PatientID { get; set; }
-        public string HospitalNumber { get; set; }
-        public string AccountNumber { get; set; }
-        public object SocialSecurity { get; set; }
-        public int PersonID { get; set; }
-        public string Lastname { get; set; }
-        public string Firstname { get; set; }
-        public int DateOfBirth { get; set; }
-        public string Gender { get; set; }
-    }*/
-
     class Program
     {
         static void Main(string[] args)
@@ -32,8 +19,6 @@ namespace Task2
             {
                 new Patient("Mustermann", "Max", "0000000001", "2017000001", 19600802, 123402081960, 'M'),
                 new Patient("Test", "Heinz", "0000000002", "2017000002", 19700901, 987601091970, 'M'),
-                /*new Person("Testperson", "Harald", 19850613, 'M'),
-                new Person("Testperson2", "Richard", 19850613, 'M'),*/
                 new Patient("Test", "Markus", "0000000003", "2017000003", 19920403, 9876003041992, 'M')
             };
             
