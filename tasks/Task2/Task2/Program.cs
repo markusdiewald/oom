0815d2b5f0742f1623713d99c0c65b51c7cd9df9
Markus Diewald <markuks.diewald@hotmail.com>
2017-03-21 20:31:05 +0100

    added Person objects for the mixed array and changed json deserialization accordingly

--- tasks/Task2/Task2/Program.cs
+++ tasks/Task2/Task2/Program.cs
@@ -19,24 +19,20 @@ namespace Task2
             {
                 new Patient("Mustermann", "Max", "0000000001", "2017000001", 19600802, 123402081960, 'M'),
                 new Patient("Test", "Heinz", "0000000002", "2017000002", 19700901, 987601091970, 'M'),
-                new Patient("Test", "Markus", "0000000003", "2017000003", 19920403, 9876003041992, 'M')
+                new Patient("Test", "Markus", "0000000003", "2017000003", 19920403, 9876003041992, 'M'),
+                new Person("Doe", "John", 19600101, 'M'),
+                new Person("Test", "Person", 19801121, 'W')
             };
-            
 
-            using (StreamWriter file = File.CreateText(@"C:\temp\patients.json"))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(file, patients);
-            }
-
-            Patient[] res = JsonConvert.DeserializeObject<Patient[]>(File.ReadAllText(@"C:\temp\patients.json"));
+            var jsonsettings = new JsonSerializerSettings() { Formatting = Formatting.Indented, TypeNameHandling = TypeNameHandling.Auto };
+            Console.WriteLine(JsonConvert.SerializeObject(patients, jsonsettings));
             
-            foreach(var x in res)
-            {
-                x.PrintDemographicData();
-            }                
+            var text = JsonConvert.SerializeObject(patients, jsonsettings);
+            File.WriteAllText(@"C:\temp\patients.json", text);
+                        
+            var textFromFile = File.ReadAllText(@"C:\temp\patients.json");
+            var itemsFromFile = JsonConvert.DeserializeObject<IDemographicInfo[]>(textFromFile, jsonsettings);
+            foreach (var x in itemsFromFile) x.PrintDemographicData();    
         }
-    }
-
-
+    }    
 }
